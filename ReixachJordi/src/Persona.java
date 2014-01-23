f3a22d9c0083de63b91c30a45038b01c5ee7faf6
Jordi Reixach <reixowtf@172.24.1.8>
2014-01-23 19:33:57 +0100

    2 commit

---
+++ ReixachJordi/src/Persona.java
@@ -0,0 +1,18 @@
+
+public class Persona {
+
+	String nom;
+
+	public Persona(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+}
